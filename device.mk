b95516ad8abbbf43bb7e550406994302164bac3a
Archie Meng <archiemeng@protonmail.com>
2025-11-03 22:03:35 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -401,3 +401,4 @@ PRODUCT_PACKAGES += \
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/fairphone/FP5/FP5-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
